21ec2b6fd579afed5f4653d061cf3fe3f4a445e7
jayas <hey@jayas.me>
2024-04-14 07:11:06 +0530

    Refresh

---
+++ src/lib/components/Hint.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { CoordVector } from "$lib/Vector"
+  import { game } from "$lib/game"
+  import type { Writable } from "svelte/store"
+
+  export let coord: CoordVector
+  export let mousePos: Writable<{ x: number; y: number }>
+
+  $: color = coord.toColor($game.difficulty)
+</script>
+
+<div
+  class="color"
+  style:top={$mousePos.x}
+  style:left={$mousePos.y}
+  style:background-color={color.toString()}
+/>
+
+<style>
+  .color {
+    position: absolute;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 0.25rem;
+  }
+</style>
